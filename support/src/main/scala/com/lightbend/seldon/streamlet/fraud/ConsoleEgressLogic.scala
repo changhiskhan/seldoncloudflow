f17a38472f035c5174d4325668d405747633a68c
blublinsky <boris.lublinsky@lightbend.com>
2020-03-23 16:24:52 -0500

    Small code optimization

---
+++ support/src/main/scala/com/lightbend/seldon/streamlet/fraud/ConsoleEgressLogic.scala
@@ -0,0 +1,53 @@
+package com.lightbend.seldon.streamlet.fraud
+
+import cloudflow.akkastream.AkkaStreamletContext
+import cloudflow.akkastream.scaladsl.RunnableGraphStreamletLogic
+import cloudflow.streamlets.CodecInlet
+import tensorflow.modelserving.avro._
+import tensorflow.support.avro._
+
+/**
+ * Abstraction for writing to output to the console (i.e., stdout).
+ *
+ * @param inlet CodecInlet for records of type ServingResult
+ */
+case class ConsoleEgressLogic(
+  inlet: CodecInlet[ServingResult], message: String)
+  (implicit context: AkkaStreamletContext)
+  extends RunnableGraphStreamletLogic {
+
+  // Write model serving results
+  private def write(record: ServingResult): Unit = {
+    println(message)
+    // Inputs
+    println("inputs:")
+    record.inputRecord.inputs.foreach {
+      case (name, tensor) ⇒ write(name, tensor)
+    }
+
+    // Results
+    println("results:")
+    record.modelResult.outputs.foreach {
+      case (name, tensor) ⇒ write(name, tensor)
+    }
+
+    // Metadata
+    println(s"execution metadata: ${record.modelResultMetadata.toString}")
+  }
+
+  // write tensor name pairs
+  private def write(name: String, t: Tensor): Unit = {
+    val data = t.dtype match {
+      case DataType.DT_FLOAT ⇒ t.float_data.get.map(_.toString)
+      case DataType.DT_DOUBLE ⇒ t.double_data.get.map(_.toString)
+      case DataType.DT_INT64 ⇒ t.long_data.get.map(_.toString)
+      case DataType.DT_INT32 ⇒ t.int_data.get.map(_.toString)
+      case DataType.DT_BOOL ⇒ t.boolean_data.get.map(_.toString)
+      case _ ⇒ t.string_data.get
+    }
+    println(s"$name -> $data")
+  }
+
+  // Runnable graph
+  def runnableGraph = sourceWithOffsetContext(inlet).map(write(_)).to(sinkWithOffsetContext)
+}
--- support/src/main/scala/com/lightbend/seldon/streamlet/fraud/FraudResultConsoleEgressTensor.scala
+++ support/src/main/scala/com/lightbend/seldon/streamlet/fraud/FraudResultConsoleEgressTensor.scala
@@ -1,11 +1,9 @@
 package com.lightbend.seldon.streamlet.fraud
 
 import cloudflow.akkastream.AkkaStreamlet
-import cloudflow.akkastream.scaladsl.RunnableGraphStreamletLogic
 import cloudflow.streamlets.StreamletShape
 import cloudflow.streamlets.avro.AvroInlet
-import tensorflow.modelserving.avro.ServingResult
-import tensorflow.support.avro.{ DataType, Tensor }
+import tensorflow.modelserving.avro._
 
 final case object FraudResultConsoleEgressTensor extends AkkaStreamlet {
 
@@ -15,41 +13,5 @@ final case object FraudResultConsoleEgressTensor extends AkkaStreamlet {
   final override val shape = StreamletShape.withInlets(in)
 
   // Create logic
-  override def createLogic = new RunnableGraphStreamletLogic() {
-
-    // Runnable graph
-    def runnableGraph = sourceWithOffsetContext(in).map(write(_)).to(sinkWithOffsetContext)
-  }
-
-  // Write model serving results
-  private def write(record: ServingResult): Unit = {
-    println("Fraud transaction")
-    // Inputs
-    println("inputs:")
-    record.inputRecord.inputs.foreach {
-      case (name, tensor) ⇒
-        write(name, tensor)
-    }
-    // Results
-    println("results:")
-    record.modelResult.outputs.foreach {
-      case (name, tensor) ⇒
-        write(name, tensor)
-    }
-    // Metadata
-    println(s"execution metadata: ${record.modelResultMetadata.toString}")
-  }
-
-  // write tensor name pairs
-  private def write(name: String, t: Tensor): Unit = {
-    val data = t.dtype match {
-      case DataType.DT_FLOAT  ⇒ t.float_data.get.map(_.toString)
-      case DataType.DT_DOUBLE ⇒ t.double_data.get.map(_.toString)
-      case DataType.DT_INT64  ⇒ t.long_data.get.map(_.toString)
-      case DataType.DT_INT32  ⇒ t.int_data.get.map(_.toString)
-      case DataType.DT_BOOL   ⇒ t.boolean_data.get.map(_.toString)
-      case _                  ⇒ t.string_data.get
-    }
-    println(s"$name -> $data")
-  }
+  override def createLogic = ConsoleEgressLogic(in, "Fraud transaction")
 }
